1590d3176d33494cf6725fa605e0a23e74663e28
stefan-mirilovic <56974152+stefan-mirilovic@users.noreply.github.com>
2020-08-10 16:32:53 +0200

    Initial project

---
+++ Backend/src/test/java/com/technosoft/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technosoft.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
